25586005f95d15ba152a36a2878c99fd490f0d39
Simon <oehrl@vr.rwth-aachen.de>
2018-05-04 13:15:46 +0200

    Fix version number
    #16

--- conanfile.py
+++ conanfile.py
@@ -23,7 +23,7 @@ from conans import ConanFile, CMake
 
 class contra(ConanFile):
     name = "contra"
-    version = "0.1.0"
+    version = "18.05"
     license = "Apache License, Version 2.0"
     description = """Contra, a lightweigth transport library for conduit data"""
     settings = "os", "compiler", "build_type", "arch"
