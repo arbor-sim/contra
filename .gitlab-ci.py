1f4950ac1e0d3089054893b6eba48bb9b5e15975
Simon Oehrl <simon.oehrl@gmail.com>
2018-05-07 15:49:51 +0200

    Replace conan stage with python script
    #20

---
+++ .gitlab-ci.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+valid_stages = ["conan", "camke", "build", "test", "deliver"]
+valid_os = ["Windows", "Linux", "OSX"]
+
+valid_compilers = {
+    "Windows": ["Visual Studio"],
+    "Linux": ["gcc"],
+    "OSX": ["apple-clang", "gcc"]
+}
+
+stage = ""
+operating_system = ""
+compiler = ""
+compiler_version = ""
+version = ""
+
+def envvar(name):
+    if operating_system == "Windows":
+        return "%%%s%%" % name
+    else:
+        return "$%s" % name
+
+def main(argv):
+    if (len(argv) != 6):
+        print("usage: .gitlab-ci.py [stage] [os] [compiler] [compiler_version] [version]")
+        return -1
+
+    stage = argv[1]
+    operating_system = argv[2]
+    compiler = argv[3]
+    compiler_version = argv[4]
+    version = argv[5]
+
+    if not stage in valid_stages:
+        print("Invalid stage, possible values: " + ', '.join(valid_stages))
+        return -1
+
+    if not operating_system in valid_os:
+        print("Invalid operating system, possible values: " + ', '.join(valid_os))
+        return -1
+
+    if not compiler in valid_compilers[operating_system]:
+        print("Invalid compiler, possible values: " + ', '.join(valid_compilers[operating_system]))
+        return -1
+
+    if stage == "conan":
+        # if operating_system == "Windows":
+        #     print("rmdir /s /q build")
+        print("mkdir build")
+        print("cd build")
+        if operating_system == "Linux":
+            print("export CC=gcc")
+            print("export CXX=g++")
+        print("conan remote update rwth-vr--bintray https://api.bintray.com/conan/rwth-vr/conan")
+        print("conan user -p %s -r rwth-vr--bintray %s" % (envvar("CONAN_PASSWORD"), envvar("CONAN_LOGIN_USERNAME")))
+
+        settings = []
+
+        settings.append("-s compiler=\"%s\"" % compiler)
+        settings.append("-s compiler.version=\"%s\"" % compiler_version)
+        settings.append("-s arch=x86_64")
+        settings.append("-s build_type=Release")
+
+        if compiler == "Visual Studio":
+            settings.append("-s compiler.runtime=MT")
+        elif compiler == "gcc":
+            settings.append("-s compiler.libcxx=\"libstdc++11\"")
+        elif compiler == "apple-clang":
+            settings.append("-s -s compiler.libcxx=libc++")            
+
+        print("conan install --build=missing %s .." % ' '.join(settings))
+    
+if (__name__ == "__main__"):
+    main(sys.argv)
