d4ecfaf6dde5cbb467810ee186da57649d6087a2
Simon <oehrl@vr.rwth-aachen.de>
2018-05-17 18:21:28 +0200

    Revert "Print cmake flags"
    This reverts commit c3012e5616f6fd7fe6a9298d1a9f50ac23a27fa1.

--- .gitlab-ci.py
+++ .gitlab-ci.py
@@ -112,8 +112,6 @@ def main(argv):
             cmake_flags.append('-DCMAKE_BUILD_TYPE=Release')
             cmake_flags.append('-DPY_TEST_COMMAND="%s"' % (pytest_command))
 
-        print(cmake_flags)
-
         execute('cmake', cmake_flags)
 
     elif stage == 'build':
