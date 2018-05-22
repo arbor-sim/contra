da850762a4cad5778366a88ce387b5cb38060dc3
Simon <oehrl@vr.rwth-aachen.de>
2018-05-22 10:04:48 +0200

    Rename OSX to macOS
    Apple changed the name with macOS 10.12 Sierra

--- .gitlab-ci.py
+++ .gitlab-ci.py
@@ -4,17 +4,18 @@ import os
 import sys
 
 valid_stages = ['conan', 'cmake', 'build', 'test', 'deliver']
-valid_os = ['Windows', 'Linux', 'OSX']
+valid_os = ['Windows', 'Linux', 'macOS']
 valid_compilers = {
     'Windows': ['Visual Studio'],
     'Linux': ['gcc'],
-    'OSX': ['apple-clang', 'gcc']
+    'macOS': ['apple-clang', 'gcc']
 }
 valid_channels = ['develop', 'stable']
 
 visual_studio_version_year_map = {
     '15': '2017'
 }
+
 
 def get_conan_flags(compiler, compiler_version):
     conan_flags = []
@@ -33,6 +34,7 @@ def get_conan_flags(compiler, compiler_version):
 
     return conan_flags
 
+
 def main(argv):
     if len(argv) != 5:
         print('usage: .gitlab-ci.py stage os compiler compiler_version')
@@ -48,11 +50,13 @@ def main(argv):
         sys.exit(-1)
 
     if not operating_system in valid_os:
-        print('Invalid operating system, possible values: %s' % ', '.join(valid_os))
+        print('Invalid operating system, possible values: %s' %
+              ', '.join(valid_os))
         sys.exit(-1)
 
     if not compiler in valid_compilers[operating_system]:
-        print('Invalid compiler for %s, possible values: %s' % (operating_system, ', '.join(valid_compilers[operating_system])))
+        print('Invalid compiler for %s, possible values: %s' %
+              (operating_system, ', '.join(valid_compilers[operating_system])))
         sys.exit(-1)
 
     if stage == 'conan':
@@ -61,9 +65,12 @@ def main(argv):
         if operating_system == 'Linux':
             os.system('export CC=gcc')
             os.system('export CXX=g++')
-        os.system('conan remote update rwth-vr--bintray https://api.bintray.com/conan/rwth-vr/conan')
-        os.system('conan user -p %s -r rwth-vr--bintray %s' % (os.environ['CONAN_PASSWORD'], os.environ['CONAN_LOGIN_USERNAME']))
-        os.system('conan install --build=missing %s ..' % ' '.join(get_conan_flags(compiler, compiler_version)))
+        os.system(
+            'conan remote update rwth-vr--bintray https://api.bintray.com/conan/rwth-vr/conan')
+        os.system('conan user -p %s -r rwth-vr--bintray %s' %
+                  (os.environ['CONAN_PASSWORD'], os.environ['CONAN_LOGIN_USERNAME']))
+        os.system('conan install --build=missing %s ..' %
+                  ' '.join(get_conan_flags(compiler, compiler_version)))
 
     elif stage == 'cmake':
         os.chdir('build')
@@ -71,7 +78,8 @@ def main(argv):
         cmake_flags = []
 
         if compiler == 'Visual Studio':
-            cmake_flags.append('-G "Visual Studio %s %s Win64"' % (compiler_version, visual_studio_version_year_map[compiler_version]))
+            cmake_flags.append('-G "Visual Studio %s %s Win64"' %
+                               (compiler_version, visual_studio_version_year_map[compiler_version]))
         else:
             cmake_flags.append('-DCMAKE_BUILD_TYPE=Release')
 
@@ -86,7 +94,7 @@ def main(argv):
 
     elif stage == 'test':
         os.chdir('build')
-        if operating_system == 'OSX':
+        if operating_system == 'macOS':
             os.environ['CTEST_OUTPUT_ON_FAILURE'] = '1'
         os.system('ctest -C Release')
 
@@ -94,12 +102,17 @@ def main(argv):
         channel = os.environ['channel']
         version = os.environ['version']
         if channel not in valid_channels:
-            print('Invalid channel: %s possible values: %s' % (channel, ', '.join(valid_channels)))
+            print('Invalid channel: %s possible values: %s' %
+                  (channel, ', '.join(valid_channels)))
             sys.exit(-1)
         conan_flags = ' '.join(get_conan_flags(compiler, compiler_version))
-        os.system('conan export-pkg . contra/%s@RWTH-VR/%s %s -f' % (version, channel, conan_flags))
-        os.system('conan test ./test_package contra/%s@RWTH-VR/%s %s' % (version, channel, conan_flags))
-        os.system('conan upload contra/%s@RWTH-VR/%s --all --force -r=rwth-vr--bintray ' % (version, channel))
+        os.system('conan export-pkg . contra/%s@RWTH-VR/%s %s -f' %
+                  (version, channel, conan_flags))
+        os.system('conan test ./test_package contra/%s@RWTH-VR/%s %s' %
+                  (version, channel, conan_flags))
+        os.system('conan upload contra/%s@RWTH-VR/%s --all --force -r=rwth-vr--bintray ' %
+                  (version, channel))
+
 
 if (__name__ == '__main__'):
     main(sys.argv)
