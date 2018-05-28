365159937d30c01197265209e2a1d598d79cef65
Simon Oehrl <oehrl@vr.rwth-aachen.de>
2018-05-28 04:16:28 -0700

    Add headers to conan package
    #29

--- conanfile.py
+++ conanfile.py
@@ -27,7 +27,7 @@ class contra(ConanFile):
     license = "Apache License, Version 2.0"
     description = """Contra, a lightweight transport library for conduit data"""
     settings = "os", "compiler", "build_type", "arch"
-    exports_sources = "src/*"
+    exports_sources = "*"
     url = "https://devhub.vr.rwth-aachen.de/VR-Group/contra"
 
     requires = (("catch/1.12.0@RWTH-VR/thirdparty"),
