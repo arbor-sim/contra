2826e4975653a3b3a1bf2c2c3561da238513a2dc
Jan Müller <j.mueller@vr.rwth-aachen.de>
2018-07-27 11:27:34 +0200

    add conan option

--- conanfile.py
+++ conanfile.py
@@ -30,13 +30,22 @@ class contra(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     exports_sources = "*"
     url = "https://devhub.vr.rwth-aachen.de/VR-Group/contra"
-
-    requires = (("catch/1.12.0@RWTH-VR/thirdparty"),
-                ("cpplint/e8ffd7c@RWTH-VR/thirdparty"),
-                ("cppcheck/1.84@RWTH-VR/thirdparty"),
-                ("conduit/0.3.1@RWTH-VR/thirdparty"),
-                ("boost_interprocess/1.66.0@bincrafters/testing"),
-                ("boost_python/1.66.0@bincrafters/testing"))
+    options = {"with_transport_boost_shared_memory": [True, False]}
+    default_options = "with_transport_boost_shared_memory=True"
+    
+    if (self.options.with_transport_boost_shared_memory):
+      requires = (("catch/1.12.0@RWTH-VR/thirdparty"),
+                  ("cpplint/e8ffd7c@RWTH-VR/thirdparty"),
+                  ("cppcheck/1.84@RWTH-VR/thirdparty"),
+                  ("conduit/0.3.1@RWTH-VR/thirdparty"),
+                  ("boost_interprocess/1.66.0@bincrafters/testing"),
+                  ("boost_python/1.66.0@bincrafters/testing"))
+    else:
+      requires = (("catch/1.12.0@RWTH-VR/thirdparty"),
+                    ("cpplint/e8ffd7c@RWTH-VR/thirdparty"),
+                    ("cppcheck/1.84@RWTH-VR/thirdparty"),
+                    ("conduit/0.3.1@RWTH-VR/thirdparty"),
+                    ("boost_python/1.66.0@bincrafters/testing"))
     generators = "cmake"
 
     def configure(self):
@@ -66,7 +75,8 @@ class contra(ConanFile):
 
     def package(self):
         self.copy("*.hpp", dst="include", src="contra/include")
-        self.copy("*.hpp", dst="include", src="contra_boost-shmem/include")
+        if (self.options.with_transport_boost_shared_memory):
+          self.copy("*.hpp", dst="include", src="contra_boost-shmem/include")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so*", dst="lib", keep_path=False)
@@ -74,4 +84,7 @@ class contra(ConanFile):
         self.copy("*.dylib", dst="lib", keep_path=False, symlinks=True)
 
     def package_info(self):
+      if (self.options.with_transport_boost_shared_memory):
         self.cpp_info.libs = ["contra", "contra_boost-shmem"]
+      else:
+         self.cpp_info.libs = ["contra"]
