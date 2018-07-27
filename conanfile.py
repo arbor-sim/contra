ad87b5b1e0b887d549332df435224375e4a4abf5
Jan Müller <j.mueller@vr.rwth-aachen.de>
2018-07-27 11:23:35 +0200

    trying to fix pipeline (Reverting changes)
    #38

--- conanfile.py
+++ conanfile.py
@@ -65,6 +65,7 @@ class contra(ConanFile):
     def build(self):
         cmake = CMake(self)
         cmake.definitions["WITH_TRANSPORT_BOOST_SHARED_MEMORY"] = "ON"
+        #cmake.definitions["-DWITH_TRANSPORT_BOOST_SHARED_MEMORY"] = "ON"
         cmake.configure(args='-DWITH_TRANSPORT_BOOST_SHARED_MEMORY=ON', source_folder='.')
         cmake.build()
 
