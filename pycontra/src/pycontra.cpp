fae9141f93fd6256b405ec422257727acf4b031c
Simon <oehrl@vr.rwth-aachen.de>
2018-07-31 12:57:01 +0200

    Add error for debug purposes
    #39

--- pycontra/src/pycontra.cpp
+++ pycontra/src/pycontra.cpp
@@ -55,9 +55,11 @@ BOOST_PYTHON_MODULE(_pycontra) {
   expose<contra::Relay<contra::FileTransport>>();
 #ifdef WITH_BOOST_SHARED_MEMORY_TRANSPORT
   expose<contra::Relay<contra::SharedMemoryTransport>>();
-  assert(false);
+  aso;
+  idjaoi;
+  sdjopaisjd
 #endif  // WITH_BOOST_SHARED_MEMORY_TRANSPORT
-  def("AnyNode", &AnyNode);
+      def("AnyNode", &AnyNode);
   class_<conduit::Node>("Node").def("Update", &conduit::Node::update);
 }
 SUPPRESS_WARNINGS_END
