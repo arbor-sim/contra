e97c7acf1c7f5a9389b00d63746245266d220827
Simon <oehrl@vr.rwth-aachen.de>
2018-07-31 12:55:18 +0200

    Add assert for debug purposes
    #39

--- pycontra/src/pycontra.cpp
+++ pycontra/src/pycontra.cpp
@@ -55,6 +55,7 @@ BOOST_PYTHON_MODULE(_pycontra) {
   expose<contra::Relay<contra::FileTransport>>();
 #ifdef WITH_BOOST_SHARED_MEMORY_TRANSPORT
   expose<contra::Relay<contra::SharedMemoryTransport>>();
+  assert(false);
 #endif  // WITH_BOOST_SHARED_MEMORY_TRANSPORT
   def("AnyNode", &AnyNode);
   class_<conduit::Node>("Node").def("Update", &conduit::Node::update);
