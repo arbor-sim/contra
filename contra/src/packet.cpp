584631fb3eb04f9d3b5f964ff807fc710faf7f39
Simon Oehrl <oehrl@vr.rwth-aachen.de>
2018-08-01 18:41:43 +0200

    Implement functions
    #49

--- contra/src/packet.cpp
+++ contra/src/packet.cpp
@@ -20,13 +20,57 @@
 //------------------------------------------------------------------------------
 
 #include "contra/packet.hpp"
+#include <sstream>
 
 #include <cstdint>
+#include <cstring>
+#include <string>
+#include <utility>
 #include <vector>
 
 namespace contra {
 
-std::vector<uint8_t> SerializePacket(const Packet&) { return {}; }
-Packet DeserializePacket(const std::vector<uint8_t>&) { return {}; }
+std::vector<uint8_t> SerializePacket(const Packet& packet) {
+  const std::size_t schema_size = packet.schema.size();
+  const std::size_t data_size = packet.data.size();
+  const std::size_t schema_size_offset = 0;
+  const std::size_t schema_offset = schema_size_offset + sizeof(size_t);
+  const std::size_t data_size_offset = schema_offset + schema_size;
+  const std::size_t data_offset = data_size_offset + sizeof(size_t);
+  const std::size_t message_size = data_offset + data_size;
+
+  std::vector<uint8_t> message(message_size);
+  std::memcpy(message.data() + schema_size_offset, &schema_size,
+              sizeof(schema_size));
+  std::memcpy(message.data() + schema_offset, packet.schema.c_str(),
+              schema_size);
+  std::memcpy(message.data() + data_size_offset, &data_size, sizeof(data_size));
+  std::memcpy(message.data() + data_offset, packet.data.data(),
+              sizeof(data_size));
+
+  return message;
+}
+
+Packet DeserializePacket(const std::vector<uint8_t>& message) {
+  const std::size_t schema_size_offset = 0;
+  std::size_t schema_size;
+  std::memcpy(&schema_size, message.data() + schema_size_offset,
+              sizeof(schema_size));
+
+  const std::size_t schema_offset = schema_size_offset + sizeof(size_t);
+  const char* schema_begin =
+      reinterpret_cast<const char*>(message.data() + schema_offset);
+  std::string schema(schema_begin, schema_begin + schema_size);
+
+  const std::size_t data_size_offset = schema_offset + schema_size;
+  std::size_t data_size;
+  std::memcpy(&data_size, message.data() + data_size_offset, sizeof(data_size));
+
+  const std::size_t data_offset = data_size_offset + sizeof(size_t);
+  const uint8_t* data_begin = message.data() + data_offset;
+  std::vector<uint8_t> data(data_begin, data_begin + data_size);
+
+  return {std::move(schema), std::move(data)};
+}
 
 }  // namespace contra
